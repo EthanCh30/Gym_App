6f5125c5858faccff55bc63b80fe2b17f9a47571
chen <565306120@qq.com>
2024-04-10 13:24:19 +0930

    update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,14 @@
-function App() {
+import Canvas from './canvas';
+import Customizer from './pages/Customizer';
+import Home from './pages/Home';
 
+function App() {
   return (
-    <h1 className="head-text">React App</h1>
+    <main className="app transition-all ease-in">
+      <Home />
+      <Canvas />
+      <Customizer />
+    </main>
   )
 }
 
